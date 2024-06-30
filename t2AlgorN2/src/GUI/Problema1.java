ec81e37da21a2f11c294b0fe0bb6013f178d9805
César Sánchez Guzmán <cesargsg93@gmail.com>
2024-06-30 16:12:57 -0500

    t2: problema 1 terminado

--- t2AlgorN2/src/GUI/Problema1.java
+++ t2AlgorN2/src/GUI/Problema1.java
@@ -1,21 +1,27 @@
 package GUI;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import examenT2.ArregloSueldos;
+
 
 public class Problema1 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtDatos;
+	private JTextArea txtArea = new JTextArea();
 
 	/**
 	 * Launch the application.
@@ -37,6 +43,8 @@ public class Problema1 extends JFrame {
 	 * Create the frame.
 	 */
 	public Problema1() {
+		ArregloSueldos arSueldo=new ArregloSueldos();
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 436, 414);
 		contentPane = new JPanel();
@@ -55,19 +63,62 @@ public class Problema1 extends JFrame {
 		contentPane.add(lblNewLabel);
 		
 		JButton btnAgregar = new JButton("Agregar");
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				arSueldo.adicionar(Double.parseDouble(txtDatos.getText()));
+				
+			}
+		});
 		btnAgregar.setBounds(164, 10, 89, 23);
 		contentPane.add(btnAgregar);
 		
 		JButton btnListar = new JButton("Listar");
+		btnListar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				listar(arSueldo);
+			}
+		});
 		btnListar.setBounds(263, 10, 89, 23);
 		contentPane.add(btnListar);
 		
 		JButton btnRPSM1000 = new JButton("Reemplazar primer sueldo menor que 1000");
+		btnRPSM1000.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				arSueldo.reemplazarPrimerSueldoMenorQue950();
+				listar(arSueldo);
+				if(arSueldo.posicionPrimerSueldoMenorQue1000()==-1) {
+					imprimir("No existe ningún sueldo menor que 1000");
+				}
+			}
+		});
 		btnRPSM1000.setBounds(164, 44, 243, 20);
 		contentPane.add(btnRPSM1000);
 		
-		JTextArea txtArea = new JTextArea();
+		
 		txtArea.setBounds(10, 77, 400, 287);
 		contentPane.add(txtArea);
+		
+		
+	}
+	
+	public void listar(ArregloSueldos x) {
+		imprimir("Elementos del Array"+"\n");
+		for (int i = 0; i < x.tamaño(); i++) {
+			imprimir(x.obtener(i)+""+"\n");
+		}
+		imprimir("cantidad de valores:"+"\n");
+		imprimir(x.tamaño()+""+"\n");
+		
+		if(x.posicionPrimerSueldoMenorQue1000()==-1) 
+			{
+			imprimir("NO EXISTE SUELDO"+"\n");
+			
+			}else { imprimir(String.valueOf(x.posicionPrimerSueldoMenorQue1000())+"\n");}
+		imprimir(x.sueldoMayor()+"\n");
+		
+	}
+	
+	public void imprimir(String x) {
+		txtArea.append(x);
 	}
 }
